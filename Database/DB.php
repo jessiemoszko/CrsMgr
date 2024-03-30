3aeed773aae1a820a178d36f687760eddee1bc44
yazziid <echalabi22@wooster.edu>
2024-03-30 16:53:07 -0400

    moved the error handling of the connexion from index and login to Dto avoid redundancy

--- Database/DB.php
+++ Database/DB.php
@@ -6,4 +6,10 @@ $db_name = 'CRS';
 $db_user = 'root';
 $db_pass = '';
 
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$conn) {
+    die("ERROR: Could not connect DB " . mysqli_connect_error());
+}
+
 ?>
