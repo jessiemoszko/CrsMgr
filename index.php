89591dcfd600a604d57bb68ae84898c2261dd68b
Jessie Moszkowicz <jess856@gmail.com>
2024-03-31 13:40:10 -0400

    fix css

--- index.php
+++ index.php
@@ -12,23 +12,21 @@ include 'sidebar.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="student.css">
+    <link rel="stylesheet" href="index.css">
     <title>Student</title>
 </head>
 
 <body>
-
-
-    <div class="content">
+    <main>
+    <div class="container">
         <h1>Main Content</h1>
         <p>This is the main content area.</p>
 
     </div>
+    </main>
 </body>
 
 </html>
-
-</body>
 
 <!-- <?php include 'footer.php' ?> -->
 
