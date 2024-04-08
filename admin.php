16d5906e4fd506078839ec7a682f6e19dece93bf
yazziid <echalabi22@wooster.edu>
2024-04-08 02:16:32 -0400

    Updating admin page

--- admin.php
+++ admin.php
@@ -1,48 +1,27 @@
-<?php
-$pageTitle='Admin Panel';
+<?php 
+$pageTitle = 'Admin Panel';
 
-require_once("session.php");
+require("helper_functions.php");
 require("header.php"); 
-
-function require_page($page)
-{
-    $pages = array(
-        'roles' => 'modifyingRoles.php',
-        'users' => 'modifyingUsers.php',
-        'assigningProfessors' => 'assigningProfessors.php',
-        'assigningTA' => 'assigningTA.php',
-        'assigningStudents' => 'assigningStudents.php'
-    );
-
-    if (array_key_exists($page, $pages)) {
-        $file = $pages[$page];
-        require($file);
-    } else {
-        http_response_code(404);
-    }
-}
+require("session.php"); 
 
 ?>
 
 <main>
-
     <div class="main-body">
-        <section>
-            <div class="col-left">
-                <div class="main-menu">
-                    <h2>Manage</h2>
-                    <ul class="menu-list">
-                        <li><a href="?page=roles">Roles</a></li>
-                        <li><a href="?page=users">Users</a></li>
-                    </ul>
+        <section class="col-left">
+            <div class="main-menu">
+                <h2>Manage</h2>
+                <ul class="menu-list">
+                    <li><a href="?page=roles">Roles</a></li>
+                </ul>
 
-                    <h2>Assign</h2>
-                    <ul class="menu-list">
-                        <li><a href="?page=assigningProfessors">Professors</a></li>
-                        <li><a href="?page=assigningTA">Teaching Assistants</a></li>
-                        <li><a href="?page=assigningStudents">Students</a></li>
-                    </ul>
-                </div>
+                <h2>Assign</h2>
+                <ul class="menu-list">
+                    <li><a href="?page=assign-professors">Professors</a></li>
+                    <li><a href="?page=assign-tas">Teaching Assistants</a></li>
+                    <li><a href="?page=assign-students">Students</a></li>
+                </ul>
             </div>
         </section>
 
@@ -51,12 +30,13 @@ function require_page($page)
 
                 <?php
                 if (isset($_GET['page'])) {
-                    require_page($_GET['page']);
-                } 
+                    $page = $_GET['page'];
+                    if (file_exists("roles.php")) {
+                        include("roles.php");
+                    }
+                }
                 ?>
-
             </div>
         </section>
     </div>
-
 </main>
