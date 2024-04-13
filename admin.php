482ff82d624d5ad56eb02227023eaabefa9cd812
Jessie Moszkowicz <jess856@gmail.com>
2024-04-12 21:30:31 -0400

    place script

--- admin.php
+++ admin.php
@@ -33,59 +33,64 @@ function extractInitials($name)
 </head>
 
 <header class="header">
-        <div class="header-text"><?php echo strtoupper($pageTitle) ?></div>
-        <div class="user" onclick="toggleDropdown()">
-            <div class="initials"><?php $initials = extractInitials($name);
-                                    echo $initials; ?></div>
-            <div class="dropdown" id="dropdown">
-                <a href="logout.php">Logout</a>
-            </div>
+    <div class="header-text"><?php echo strtoupper($pageTitle) ?></div>
+    <div class="user" onclick="toggleDropdown()">
+        <div class="initials"><?php $initials = extractInitials($name);
+                                echo $initials; ?></div>
+        <div class="dropdown" id="dropdown">
+            <a href="logout.php">Logout</a>
         </div>
-    </header>
+    </div>
+</header>
 
 
 <body>
 
-<div class="admin-container">
+    <div class="admin-container">
 
-    <div class="tile-group">
-        <div class="tiles">
-        <h2>Manage</h2>
-        <ul class="tiles">
-            <li><a href="?page=roles">Modifying Roles</a></li>
-            <li><a href="?page=users">Modifiying users</a></li>
-        </ul>
-        </div>
-
-        <div class="tiles">
-        <h2>Assign</h2>
-        <ul class="tiles">
-            <li><a href="?page=assign-professors">Professors</a></li>
-            <li><a href="?page=assign-tas">Teaching Assistants</a></li>
-            <li><a href="?page=assign-students">Students</a></li>
-        </ul>
-        </div>
-        <div class="tiles">
-        <h2>Home</h2>
-        <ul class="tiles">
-            <li><a href="admin.php">Home</a></li>
-        </ul>
-        </div>
-        </div>
-
-            <div>
-                <?php
-                if (isset($_GET['page'])) {
-                    $page = $_GET['page'];
-                    if (file_exists($page . ".php")) {
-                        include($page . ".php");
-                    }
-                }
-                ?>
+        <div class="tile-group">
+            <div class="tiles">
+                <h2>Manage</h2>
+                <ul class="tiles">
+                    <li><a href="?page=roles">Modifying Roles</a></li>
+                    <li><a href="?page=users">Modifiying users</a></li>
+                </ul>
             </div>
-    
-   
-    
+
+            <div class="tiles">
+                <h2>Assign</h2>
+                <ul class="tiles">
+                    <li><a href="?page=assign-professors">Professors</a></li>
+                    <li><a href="?page=assign-tas">Teaching Assistants</a></li>
+                    <li><a href="?page=assign-students">Students</a></li>
+                </ul>
+            </div>
+            <div class="tiles">
+                <h2>Home</h2>
+                <ul class="tiles">
+                    <li><a href="admin.php">Home</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div>
+            <?php
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'];
+                if (file_exists($page . ".php")) {
+                    include($page . ".php");
+                }
+            }
+            ?>
+        </div>
+
     </div>
+    <script>
+        function toggleDropdown() {
+            var dropdown = document.getElementById("dropdown");
+            dropdown.classList.toggle("show");
+        }
+    </script>
 </body>
+
 </html>
