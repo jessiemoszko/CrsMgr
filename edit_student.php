0b9de8c965899fbc40e4601c1fb3ef2a4c0a4127
Diba Pourzandi <150370590+diba-pourzandi@users.noreply.github.com>
2024-04-16 18:51:27 -0400

    edit in student_groups and not user table

--- edit_student.php
+++ edit_student.php
@@ -2,10 +2,7 @@
     require 'Database/DB.php';
 	$userID=$_GET['id'];
  
-	$firstname=$_POST['firstname'];
-	$lastname=$_POST['lastname'];
+	$groupID=$_POST['groupID'];
  
-	mysqli_query($conn,"update `user` set first_name='$firstname', last_name='$lastname' where userID='$userID'");
+	mysqli_query($conn,"update `user` set groupID='$groupID' where userID='$userID'");
 	header('Location: groups.php');
-?>
-
