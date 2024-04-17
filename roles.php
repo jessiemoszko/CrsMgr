c9f9f228d4cd633baf8fe2976868177fa38451cb
Jessie Moszkowicz <jess856@gmail.com>
2024-04-17 11:07:18 -0400

    add css

--- roles.php
+++ roles.php
@@ -17,7 +17,8 @@ function extractInitials($name)
     return $initials;
 }
 
-function addRole($conn, $role_name) {
+function addRole($conn, $role_name)
+{
     global $errors;
     global $success;
 
@@ -36,10 +37,11 @@ function addRole($conn, $role_name) {
     return array($success, $errors);
 }
 
-function updateRole($conn, $role_name, $role_id) {
+function updateRole($conn, $role_name, $role_id)
+{
     global $errors;
     global $success;
-    
+
     if (empty($role_name)) {
         array_push($errors, "Role Name is required");
     } else {
@@ -53,13 +55,13 @@ function updateRole($conn, $role_name, $role_id) {
         mysqli_stmt_close($stmt);
     }
     return array($success, $errors);
-
 }
 
-function deleteRole($conn, $role_id) {
+function deleteRole($conn, $role_id)
+{
     global $errors;
     global $success;
- 
+
     $stmt = mysqli_prepare($conn, "DELETE FROM role WHERE roleID = ?");
     mysqli_stmt_bind_param($stmt, "i", $role_id);
     if (mysqli_stmt_execute($stmt)) {
@@ -93,87 +95,102 @@ if ($addedOrUpdated) {
 }
 ?>
 
-<div class="content-body">
-    <?php display_success($success); ?>
-    <?php display_error($errors); ?>
+<!DOCTYPE html>
+<html lang="en">
 
-    <h2>Roles</h2>
-    <hr>
-    <table>
-        <thead>
-            <tr>
-                <th>Role Name</th>
-                <th>Role Description</th>
-                <?php if (isAdmin()): ?>
-                    <th colspan="2">Action</th>
-                <?php endif; ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($results as $row): ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="content-body">
+        <?php display_success($success); ?>
+        <?php display_error($errors); ?>
+
+        <h2>Roles</h2>
+        <hr>
+        <table>
+            <thead>
                 <tr>
-                    <td><?= $row['role_name'] ?></td>
-                    <?php if (isAdmin()): ?>
-                        <td><a href="?page=role&update_view=true&update_id=<?= $row['roleID'] ?>">Update</a></td>
-                        <td><a href="?page=role&delete_id=<?= $row['roleID'] ?>" onclick="return confirm('Are you sure you want to delete?')">Delete Role</a></td>
+                    <th>Role Name</th>
+                    <th>Role Description</th>
+                    <?php if (isAdmin()) : ?>
+                        <th colspan="2">Action</th>
                     <?php endif; ?>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $row) : ?>
+                    <tr>
+                        <td><?= $row['role_name'] ?></td>
+                        <?php if (isAdmin()) : ?>
+                            <td><a href="?page=role&update_view=true&update_id=<?= $row['roleID'] ?>">Update</a></td>
+                            <td><a href="?page=role&delete_id=<?= $row['roleID'] ?>" onclick="return confirm('Are you sure you want to delete?')">Delete Role</a></td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
-    <?php if (isAdmin()): ?>
-        <a href="?page=role&add_view=true">
-            <button>Add Role</button>
+        <?php if (isAdmin()) : ?>
+            <a href="?page=role&add_view=true">
+                <button>Add Role</button>
+            </a>
+
+            <?php if (isset($_GET['add_view'])) : ?>
+                <hr>
+                <div class="form-container">
+                    <form class="form-body" action="" method="POST">
+                        <h3>Add Role</h3>
+                        <div class="form-input">
+                            <label>Role Name</label>
+                            <span><input type="text" name="role_name"></span>
+                        </div>
+                        <div class="form-submit">
+                            <input type="submit" name="add_role" value="Add">
+                        </div>
+                    </form>
+                </div>
+            <?php endif; ?>
+
+            <?php if (isset($_GET['update_view'])) : ?>
+                <?php $id = mysqli_real_escape_string($conn, $_GET['update_id']);
+                $query = "SELECT * FROM role WHERE roleID='$id'";
+                $result = mysqli_query($conn, $query);
+
+                if (mysqli_num_rows($result) > 0) {
+                    $row = mysqli_fetch_assoc($result);
+                    $role_name = $row['role_name'];
+                } else {
+                    $role_name = "";
+                }
+                ?>
+                <div class="form-container">
+                    <form class="form-body" action="" method="POST">
+                        <h3>Update Role</h3>
+                        <div class="form-input">
+                            <label>Role Name</label>
+                            <span><input type="text" name="role_name" value="<?= $role_name ?>"></span>
+                        </div>
+                        <div class="form-submit">
+                            <input type="submit" name="update_role" value="Update">
+                        </div>
+                    </form>
+                </div>
+            <?php endif; ?>
+        <?php endif; ?>
+        <a href="admin.php">
+            <button>Back to Admin Panel</button>
         </a>
-
-        <?php if (isset($_GET['add_view'])): ?>
-            <hr>
-            <div class="form-container">
-                <form class="form-body" action="" method="POST">
-                    <h3>Add Role</h3>
-                    <div class="form-input">
-                        <label>Role Name</label>
-                        <span><input type="text" name="role_name"></span>
-                    </div>
-                    <div class="form-submit">
-                        <input type="submit" name="add_role" value="Add">
-                    </div>
-                </form>
-            </div>
-        <?php endif; ?>
-
-        <?php if (isset($_GET['update_view'])): ?>
-            <?php $id = mysqli_real_escape_string($conn, $_GET['update_id']);
-            $query = "SELECT * FROM role WHERE roleID='$id'";
-            $result = mysqli_query($conn, $query);
-
-            if (mysqli_num_rows($result) > 0) {
-                $row = mysqli_fetch_assoc($result);
-                $role_name = $row['role_name'];
-            } else {
-                $role_name = "";
-            }
-            ?>
-
-        <div class="form-container">
-        <form class="form-body" action="" method="POST">
-            <h3>Update Role</h3>
-            <div class="form-input">
-                <label>Role Name</label>
-                <span><input type="text" name="role_name" value="<?= $role_name ?>"></span>
-            </div>
-            <div class="form-submit">
-                <input type="submit" name="update_role" value="Update">
-            </div>
-        </form>
+        <a href="roles.php">
+            <button>Refresh</button>
+        </a>
     </div>
-        <?php endif; ?>
-    <?php endif; ?>
-    <a href="admin.php">
-        <button>Back to Admin Panel</button>
-    </a>
-    <a href="roles.php">
-        <button>Refresh</button>
-    </a>
-</div>
+
+</body>
+
+</html>
