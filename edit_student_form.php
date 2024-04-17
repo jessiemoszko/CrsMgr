da577be3cd643e501fa32aad8771a9d8ef637be3
Diba Pourzandi <150370590+diba-pourzandi@users.noreply.github.com>
2024-04-17 11:05:35 -0400

    stylize edit_form

--- edit_student_form.php
+++ edit_student_form.php
@@ -13,10 +13,34 @@
 <body>
 	<h2>Edit</h2>
 	<form method="POST" action="edit_student.php?id=<?php echo $id; ?>">
-		<label>Group Name:</label><input type="text" value="<?php echo $row['groupID']; ?>" name="groupID">
+		<label>Group Name:</label><input type="text" value="<?php echo $row['groupID']; ?>" name="groupID"><br>
 		<input type="submit" name="submit">
 		<a href="groups.php" class="back_button">Back</a>
 	</form>
 </body>
+
+<style>
+
+input[type="text"] {
+	width: 50px;
+}
+
+.back_button {
+   font-family: 'Karla', sans-serif;
+   background-color:  #29333F; 
+   border: none;
+   color: white;
+   padding: 5px 10px 5px 10px;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   font-size: 16px;
+   margin: 15px 2px;
+   cursor: pointer;
+   border-radius: 10px
+   
+}
+
+</style>
 </html>
 
