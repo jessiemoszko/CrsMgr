78a69288815cc6e127f6d9cc1bd7c629ce141551
Diba Pourzandi <150370590+diba-pourzandi@users.noreply.github.com>
2024-04-17 10:51:29 -0400

    add styling css to groups.php

--- groups.php
+++ groups.php
@@ -22,7 +22,6 @@ $result = mysqli_query($conn,$query);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="groups.css">
   <title>Student Groups</title>
 </head>
 
@@ -30,11 +29,8 @@ $result = mysqli_query($conn,$query);
 <body>
 
   <div class="container">
-    <div class = "row mt-5">
-    <div class="col">
-      <div class="card mt-5">
       <?php if (isProfessor() || isAdmin() || isTA()) { ?>
-      <div class="card-header"><a class="btn btn-primary" href="add_student_groups.php">Add Student</a></div>
+      <div class="add-student"><a class="btn btn-primary" href="add_student_groups.php">Add Student</a></div>
       <?php } ?>
         <div class="card-body">
       <table class="table table-bordered text-center">
@@ -85,9 +81,51 @@ while($row = mysqli_fetch_assoc($result)){
 ?>
 </table>
 </div>
-</div>
-</div>
-</div>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+.container {
+    margin-top: 80px;
+    margin-left: 250px;
+    width: 100%;
+    position: relative;
+    box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.);
+    border-radius: 0px;
+    border-collapse: collapse;
+    overflow: hidden;
+}
+
+body {
+  background-color: #eff4ff;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px; }
+
+button[type=submit], .btn {
+   font-family: 'Karla', sans-serif;
+   background-color:  #29333F; 
+   border: none;
+   color: white;
+   padding: 5px 10px 5px 10px;
+   text-align: center;
+   text-decoration: none;
+   display: inline-block;
+   font-size: 16px;
+   margin: 4px 2px;
+   cursor: pointer;
+   border-radius: 10px
+}
+
+.add-student {
+  margin-bottom:10px;
+}
+
+
+
+</style>
 
 </body>
 </html>
