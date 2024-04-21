ee76359ac1a75b0c5df52a2075bedbe61d5c35c1
yazziid <echalabi22@wooster.edu>
2024-04-21 16:06:27 -0400

    Adding page directions to TA

--- TA.php
+++ TA.php
@@ -21,21 +21,62 @@ require 'sidebar.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+
+<<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css">
-    <title>TA</title>
+    <link rel="stylesheet" href="admin.css">
+    <title>TA Panel</title>
 </head>
-<body>
-    <main>
-        <div class="container">
-            <h1>Main Content</h1>
-            <p>This is the main content area.</p>
 
+<header class="header">
+
+    <div class="header-text">
+        <?php echo strtoupper($pageTitle) ?>
+    </div>
+    
+    <div class="user" onclick="toggleDropdown()">
+
+        <div class="initials"><?php $initials = extractInitials($name);
+                                echo $initials; ?>
         </div>
+        
+        <div class="dropdown" id="dropdown">
+            <a href="logout.php">Logout</a>
+        </div>
+    
+    </div>
 
-    </main>
+</header>
+
+<body>
+
+    <div class="admin-container">
+
+        <div class="tile-group">
+            
+            <div class="tiles-large">
+
+                <h2>Manage</h2>
+                
+                <ul class="tiles">
+                     <li><a href="groups.php">Modifiy Groups</a></li>
+                     <li><a href="announcements.php">Modifiy Announcements</a></li>
+                     <li><a href="assignments.php">Modifiy Assignments</a></li>
+                     <li><a href="course_material.php">Modifiy Course Material</a></li>
+                     <li><a href="FAQ.php">Modifiy FAQ</a></li>
+                     <li><a href="grades.php">Modifiy Grades</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function toggleDropdown() {
+            var dropdown = document.getElementById("dropdown");
+            dropdown.classList.toggle("show");
+        }
+    </script>
 </body>
+
 </html>
-<?php require("footer.php"); ?>
